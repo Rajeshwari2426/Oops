9cdfe5cbf1c02b3b2b410b0a7bdd76ded1b17fc9
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-28 15:40:42 +0530

    [Rajeshwari] adding Constructor

---
+++ OopsConcepts/OopsConcepts/Constructor/CarConstruct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts.Constructor
+{
+    class CarConstruct
+    {
+        public string model, color, type;
+        public int maxSpeed;
+
+        public CarConstruct(string modelName, string colorName, string typeName, int max_Speed)
+        {
+            model = modelName;
+            color = colorName;
+            type = typeName;
+            maxSpeed = max_Speed;
+        }
+    }
+    
+}
--- OopsConcepts/OopsConcepts/Program.cs
+++ OopsConcepts/OopsConcepts/Program.cs
@@ -7,6 +7,7 @@ using OopsConcepts.Abstraction;
 using OopsConcepts.Polymorphism;
 using OopsConcepts.Inheritence;
 using OopsConcepts.Encapsulation;
+using OopsConcepts.Constructor;
 
 namespace OopsConcepts
 {
@@ -14,6 +15,13 @@ namespace OopsConcepts
     {
         static void Main(string[] args)
         {
+            // Constructor -----------------------------------------------------------
+            CarConstruct honda = new CarConstruct("City", "Black", "Sedan", 200);
+
+            Console.WriteLine($"New car is Honda {honda.model}, Color:{honda.color} with max Speed {honda.maxSpeed} & type {honda.type}");
+
+
+
             // Encapsulation -------------------------------------------------------
             Encapsulating person = new Encapsulating();
             person.Name = "Tony";
