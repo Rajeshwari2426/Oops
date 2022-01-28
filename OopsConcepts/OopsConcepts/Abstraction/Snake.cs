18e3e8607d69b02f71c1b9031f6f5093575da980
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-28 13:24:25 +0530

    [Rajeshwari] adding Abstraction

---
+++ OopsConcepts/OopsConcepts/Abstraction/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OopsConcepts.Abstraction;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts.Abstraction
+{
+    class Snake : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The snake sounds Hissss");
+        }
+    }
+    
+}
---
+++ OopsConcepts/OopsConcepts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OopsConcepts.Abstraction;
+
+namespace OopsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstraction-------------------------------------------------------------
+            
+            Snake mySnake = new Snake();
+            mySnake.animalSound();
+            mySnake.sleep();
+
+
+            Console.ReadLine();
+        }
+    }
+}
